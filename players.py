5eb7f991e77224cb668974f2844b9477699d5ceb
Christopher Lee <leec@chem.ucla.edu>
2013-11-29 13:10:24 -0800

    added standard players definitions

---
+++ players.py
@@ -0,0 +1,10 @@
+import info
+
+tft = (1.,0.,1.,0.)
+wsls = (1.,0.,0.,1.)
+alld = (0.,0.,0.,0.)
+allc = (1.,1.,1.,1.)
+zdx = info.zd_vector1(2.)
+zdgtft2 = info.zd_vector2(2.)
+
+__all__ = ['tft', 'wsls', 'alld', 'allc', 'zdx', 'zdgtft2']
