da310bae1c140bee4cdb50e6326ace2a20d5e098
Christopher Lee <leec@chem.ucla.edu>
2013-12-17 19:42:13 -0800

    added zdr_vector() and zdr2 player for donation game

--- players.py
+++ players.py
@@ -6,5 +6,6 @@ alld = (0.,0.,0.,0.)
 allc = (1.,1.,1.,1.)
 zdx = info.zd_vector1(2.)
 zdgtft2 = info.zd_vector2(2.)
+zdr2 = info.zdr_vector(0.5)
 
-__all__ = ['tft', 'wsls', 'alld', 'allc', 'zdx', 'zdgtft2']
+__all__ = ['tft', 'wsls', 'alld', 'allc', 'zdx', 'zdgtft2', 'zdr2']
