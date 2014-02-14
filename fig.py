e095fa02ce2331067d48eee617bb3df1981e5744
Christopher Lee <leec@chem.ucla.edu>
2014-02-14 14:44:49 -0800

    added ConSwitch tag-player to chi-epsilon zd robustness fig

--- fig.py
+++ fig.py
@@ -177,11 +177,13 @@ def read_csv(csvfile):
         return [t[:2] + [int(i) for i in t[2:6]] + [float(x) for x in t[6:]]
                 for t in csv.reader(ifile)]
 
-def plot_zd_selection(data, filterFunc, label=None, dy=0):
+def plot_zd_selection(data, filterFunc=lambda t:True, label=None, dy=0, 
+                      marker='o', **kwargs):
     l = [(t[-2], (float(t[4]) / t[5]) / (float(t[2]) / t[3])) for t in data
          if filterFunc(t)]
     l.sort()
-    pyplot.semilogy([t[0] for t in l], [t[1] for t in l], marker='o')
+    pyplot.semilogy([t[0] for t in l], [t[1] for t in l], marker=marker, 
+                    **kwargs)
     if label:
         pyplot.text(l[0][0], l[0][1] + dy, label)
 
@@ -189,7 +191,7 @@ def plot_zd_data(data):
     plot_zd_selection(data, lambda t:t[-1]==2 and t[-3]==0. and t[-2]>=0, 
                       '$\epsilon=0$')
     plot_zd_selection(data, lambda t:t[-1]==2 and t[-3]==0.01 and t[-2]>=0, 
-                      '$\epsilon=0.01$')
+                      '$\epsilon=0.01$', -0.3)
     plot_zd_selection(data, lambda t:t[-1]==2 and t[-3]==0.05 and t[-2]>=0, 
                       '$\epsilon=0.05$', -1)
     plot_zd_selection(data, lambda t:t[-1]==2 and t[-3]==0.1 and t[-2]>=0, 
@@ -201,9 +203,12 @@ def plot_zd_data(data):
     pyplot.ylabel('IP0 invasion success')
     pyplot.tight_layout()
 
-def zd_robustness_fig(csvfile='zd_results.csv'):
+def zd_robustness_fig(csvfile='zd_results.csv', tagfile='tag_results.csv'):
     data = read_csv(csvfile)
     plot_zd_data(data)
+    data = read_csv(tagfile)
+    plot_zd_selection(data, label='ConSwitch ($\epsilon=0$)', marker='^', 
+                      linestyle='--', color='k')
     
 def plot_identify_cdf(pvec, n=10000, label='', **kwargs):
     l = [info.time_to_identify(pvec,  **kwargs) for i in range(n)]
