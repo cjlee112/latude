5c1790f86fdf51c3c167c4b38b1d8806466a5b61
Christopher Lee <leec@chem.ucla.edu>
2014-05-05 15:39:05 -0700

    added basic MaRS test and raised AP2 to 5 as given in the MaRS paper

--- mars.py
+++ mars.py
@@ -1,36 +1,33 @@
 import collections
 
+START = 0
 ENACT = 1
 EXPECT = 2
 EXCLUDE = 3
 
 class MarsStrategy(object):
-    def __init__(self, scores=(2, -1, 3, 0), m1=7, m2=5, ap1=2, ap2=2, 
+    def __init__(self, scores=(2, -1, 3, 0), m1=7, m2=5, ap1=2, ap2=5, 
                  pvec=None):
         r,s,t,p = scores
         self.pstar = float(t - s) / (t - s + r - p)
         self.passive = collections.deque(maxlen=m1)
         self.reactive = collections.deque(maxlen=m2)
-        self.state = 0
+        self.state = START
         self.step = 0
         self.monitor = False
         self.ap1 = ap1
         self.ap2 = ap2
+        self.p_sp = self.p_sr = 0.
 
     def next_move(self, epsilon=0.05):
         if len(self.passive) < self.ap1: # passive EMPTY
             return 'C'
-        p_sp = float(sum(self.passive)) / len(self.passive)
-        if len(self.reactive) >= self.ap2:
-            p_sr = float(sum(self.reactive)) / len(self.reactive)
-        else: # reactive EMPTY
-            p_sr = 1.
         if self.state == EXPECT and self.step == 1: # always cooperate 2nd step
             return 'C'
-        elif p_sp < self.pstar:
+        elif self.p_sp < self.pstar:
             self.state = EXCLUDE
             return 'D'
-        elif p_sr >= self.pstar and self.last_move == 'DD':
+        elif self.p_sr >= self.pstar and self.last_move == 'DD':
             self.state = EXPECT
             self.step = 0
             return 'C'
@@ -38,12 +35,13 @@ class MarsStrategy(object):
             self.state = ENACT
             return self.last_move[1]
 
-    def print_state(self, p_sp, p_sr, 
+    def print_state(self,
                     states=('init', 'enact', 'expect', 'exclude')):
         print 'state: %s, %s, %s, %0.3f, %0.3f, %d' \
             % (states[self.state], 
                ''.join([str(i) for i in self.passive]), 
-               ''.join([str(i) for i in self.reactive]), p_sp, p_sr, self.step)
+               ''.join([str(i) for i in self.reactive]), 
+               self.p_sp, self.p_sr, self.step)
 
     def save_outcome(self, last_move):
         self.last_move = last_move
@@ -52,6 +50,7 @@ class MarsStrategy(object):
                 self.passive.append(1)
             else:
                 self.passive.append(0)
+            self.p_sp = float(sum(self.passive)) / len(self.passive)
         if self.state == EXPECT and self.step == 1:
             if last_move[1] == 'C':
                 self.reactive.append(1)
@@ -61,6 +60,10 @@ class MarsStrategy(object):
         elif self.monitor and last_move[1] == 'D':
             self.reactive.append(-1)
             self.monitor = False
+        if len(self.reactive) >= self.ap2:
+            self.p_sr = float(sum(self.reactive)) / len(self.reactive)
+        else: # reactive EMPTY
+            self.p_sr = 1.
         self.step += 1
 
 
